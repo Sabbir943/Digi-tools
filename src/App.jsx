e3f3e4269a2cf5ba92a271faaf75d0d22c4b73de
sabbir--ahmad <mdsabbirahmad943@gmail.com>
2026-03-30 20:16:19 +0600

    done by user section by responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import HeroSection from './Component/HeroSection/HeroSection'
 import NavBar from './Component/NavBar/NavBar'
+import User from './Component/UserSection/User'
 function App() {
  return (
     <>
    <NavBar></NavBar>
    <HeroSection></HeroSection>
+   <User></User>
 
 
     
---
+++ src/Component/UserSection/User.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div className='bg-[#4F39F6] mb-20'>
+            <div className='text-white flex justify-between items-center w-8/12 mx-auto p-8'>
+                <div>
+                    <p className='text-4xl font-bold mb-3'>50K+</p>
+                    <p>Active Users</p>
+                </div>
+                <div>
+                    <p className='text-4xl font-bold mb-3'>200+</p>
+                    <p>Premium Tools</p>
+                </div>
+                <div>
+                    <p className='text-4xl font-bold mb-3'>4.9</p>
+                    <p>Rating</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default User;
